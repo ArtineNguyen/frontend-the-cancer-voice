1c6e673bf0f2ac90aa7b8ad17efff87520ccf381
artine <artinenguyen@gmail.com>
2019-12-31 14:54:52 +0700

    fix sildebar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
-import Slidebar from '../components/Slidebar'
+// import Slidebar from '../components/Slidebar'
 import AboutUs from '../components/AboutUs'
 import Footer from '../pages/Footer'
 import About from '../pages/About'
@@ -68,7 +68,7 @@ export default function Home(props) {
 
 	return (
 		<>
-			<Slidebar />
+			{/* <Slidebar /> */}
 			<AboutUs />
 			<Blog/>
 			

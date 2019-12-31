d5bc2bfa965fa88750fbf3390b23ce46ae212384
artine <artinenguyen@gmail.com>
2019-12-31 14:52:12 +0700

    im here

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
-import Slidebar from "../components/Slidebar"
+import Slidebar from '../components/Slidebar'
 import AboutUs from '../components/AboutUs'
 import Footer from '../pages/Footer'
 import About from '../pages/About'
